a51a76952ec6ff863082867ffbd6a0e54ea255e2
Gabriela Nogueira <snogueiragabriela@gmail.com>
2016-02-20 22:38:00 -0200

    first commit

---
+++ desafio/desafio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  desafio
+//
+//  Created by Gabriela Nogueira on 2/20/16.
+//  Copyright © 2016 Gabriela Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ desafio/desafioUITests/desafioUITests.m
@@ -0,0 +1,40 @@
+//
+//  desafioUITests.m
+//  desafioUITests
+//
+//  Created by Gabriela Nogueira on 2/20/16.
+//  Copyright © 2016 Gabriela Nogueira. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface desafioUITests : XCTestCase
+
+@end
+
+@implementation desafioUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+@end
